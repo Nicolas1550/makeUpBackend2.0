5321f89c3fd39c9212f8a4f51c7113b53bee7a6e
Tu Nombre <tuemail@example.com>
2024-08-21 18:14:58 -0300

    f

--- src/db.js
+++ src/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  port: process.env.DB_PORT || 3306,  // Utiliza el puerto proporcionado por Ngrok o el puerto predeterminado de MySQL
   logging: false,
 });
 
@@ -18,4 +19,3 @@ async function dbConnect() {
 }
 
 module.exports = { sequelize, dbConnect };
-
