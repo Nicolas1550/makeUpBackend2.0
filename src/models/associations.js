ba12c83f518bbedb149117dca423b76f9dc9e20a
Tu Nombre <tuemail@example.com>
2024-09-19 18:31:56 -0300

    ff

--- src/models/associations.js
+++ src/models/associations.js
@@ -4,22 +4,21 @@ const OrderProducts = require('./OrderProducts');
 const User = require('./User');
 const Servicio = require('./Services');
 const Role = require('./Role');
-const UserServices = require('./UserServices'); // Asegúrate de que el modelo UserServices esté definido
 
-// Define asociaciones para Product y ProductOrder (relación muchos a muchos a través de OrderProducts)
-Product.belongsToMany(ProductOrder, { through: OrderProducts, as: 'productOrdersAssociation', foreignKey: 'ProductId' });
-ProductOrder.belongsToMany(Product, { through: OrderProducts, as: 'orderProductsAssociation', foreignKey: 'ProductOrderId' });
+// Define asociaciones para Product y ProductOrder (muchos a muchos a través de OrderProducts)
+Product.belongsToMany(ProductOrder, { through: OrderProducts, as: 'productOrdersAssociation' });
+ProductOrder.belongsToMany(Product, { through: OrderProducts, as: 'orderProductsAssociation' });
 
 // Define relación uno a muchos entre ProductOrder y User
-ProductOrder.belongsTo(User, { foreignKey: 'user_id', as: 'userOrderAssociation' });
+ProductOrder.belongsTo(User, { as: 'userOrderAssociation' }); // Elimina foreignKey para permitir a Sequelize manejarlo
 
-// Define la asociación muchos a muchos entre User y Servicio usando la tabla "UserServices"
-User.belongsToMany(Servicio, { through: UserServices, as: 'userServicesAssociation', foreignKey: 'UserId' });
-Servicio.belongsToMany(User, { through: UserServices, as: 'serviceUsersAssociation', foreignKey: 'ServicioId' });
+// Define la asociación muchos a muchos entre User y Servicio sin foreignKey explícitas
+User.belongsToMany(Servicio, { through: 'UserServices', as: 'userServicesAssociation' });
+Servicio.belongsToMany(User, { through: 'UserServices', as: 'serviceUsersAssociation' });
 
-// Define la relación muchos a muchos entre User y Role usando la tabla "user_roles"
-User.belongsToMany(Role, { through: 'user_roles', as: 'userRolesAssociation', foreignKey: 'userId' });
-Role.belongsToMany(User, { through: 'user_roles', as: 'roleUsersAssociation', foreignKey: 'roleId' });
+// Relación muchos a muchos entre User y Role sin foreignKey explícitas
+User.belongsToMany(Role, { through: 'user_roles', as: 'userRolesAssociation' });
+Role.belongsToMany(User, { through: 'user_roles', as: 'roleUsersAssociation' });
 
 // Exportar los modelos con las asociaciones definidas
-module.exports = { Product, ProductOrder, OrderProducts, User, Servicio, Role, UserServices };
+module.exports = { Product, ProductOrder, OrderProducts, User, Servicio, Role };
